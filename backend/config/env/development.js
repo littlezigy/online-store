2e9d0be14d6c9f852ec07afd0250492e1cdb5f98
Adesuwa Dawodu <adesuwa.tee@gmail.com>
2019-02-22 14:36:43 +0100

    Added MONGODB_URI var for local dev

--- backend/config/env/development.js
+++ backend/config/env/development.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
+  MONGODB_URI='',
   /***************************************************************************
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-  models: {},
+  models: {
+    datastore: 'productionMongo',
+    attributes: {
+      createdAt: { type: 'number', autoCreatedAt: true, },
+      updatedAt: { type: 'number', autoUpdatedAt: true, },
+      id: { type: 'number', columnName: '_id'}, // <-- for MongoDB
+    },
+    schema: true,
+  },
 
   //   /****** twilio ******/
 
